638fca9aabb3dcf50cb36d73f717d1ed43e0f79a
chrisvasqm <christianvasquezmelo@gmail.com>
2022-11-01 10:11:32 -0400

    Add padding

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
           Navbar
-          <span className="badge badge-pill badge-secondary">
+          <span className="badge badge-pill badge-secondary m-2">
             {this.props.totalCounters}
           </span>
         </a>
